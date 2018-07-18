8bd0cc58cfcc61a222a31af7ff1f4c0f052f29bb
Racter <paracetamolboy@gmail.com>
2018-07-18 12:59:58 +0200

    Ran formatter on project. Changed formatter configuration.

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,5 @@
 # Used by "mix format"
-
-# The default glob pattern matching doesn't match files starting with a '.'
-inputs = ["*.exs", "{config,lib,test}/**/*.{ex,exs}"] ++ Path.wildcard(".*.exs", match_dot: true)
-
 [
-  import_deps: [:grpc, :protobuf],
-  inputs: inputs,
-  locals_without_parens: [xds_tests: 2, plug: 1]
+  import_deps: [:ecto, :plug],
+  inputs: ["*.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]

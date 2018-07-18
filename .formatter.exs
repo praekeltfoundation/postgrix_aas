529cd51f7d2a496aca1480df39916a9a6d84c36e
Racter <paracetamolboy@gmail.com>
2018-07-18 12:49:18 +0200

    Added configuration file for formatter.

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
 # Used by "mix format"
+
+# The default glob pattern matching doesn't match files starting with a '.'
+inputs = ["*.exs", "{config,lib,test}/**/*.{ex,exs}"] ++ Path.wildcard(".*.exs", match_dot: true)
+
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  import_deps: [:grpc, :protobuf],
+  inputs: inputs,
+  locals_without_parens: [xds_tests: 2, plug: 1]
 ]

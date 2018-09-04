24a759f7f12025da4e45f78cedcf501a7607e167
Racter <paracetamolboy@gmail.com>
2018-09-04 12:25:23 +0200

    Change sleep from :timer.sleep to Process.sleep

--- test/postgrix_cluster_test.exs
+++ test/postgrix_cluster_test.exs
@@ -137,7 +137,7 @@ defmodule PostgrixCluster.Test do
 
     API.addOwnerRole(pid, db_name, db_owner, owner_pass)
     #Sleep to avoid race condition where check is made before transaction completes
-    :timer.sleep(100)
+    Process.sleep(100)
     assert API.roleExists?(pid, db_owner) == true
 
     API.grantOwnerRole(pid, db_owner, vault_user)
